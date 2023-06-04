0c2068f19a7853d7b9db9e1c35d90ad42b1e557c
Matt Gray <Matt@Matt-Gray.co.uk>
2023-06-04 22:40:48 +0100

    bluetooth test updated

--- test.py
+++ test.py
@@ -3,14 +3,17 @@ from bleak import BleakScanner
 
 async def main():
     while True:
-      d = await BleakScanner.find_device_by_address('D5:5D:7C:31:DD:D5')
-      print(d)
-      ba = d.details['props']['ManufacturerData'][2409]
-      print(ba)
-      print(''.join('{:02x}'.format(x) for x in ba))
-      print(ba[9] - 128)
-      print(ba[8])
-      print(ba[10])
-      await asyncio.sleep(29)
+      devices = await BleakScanner.discover() #scanning_mode='passive',bluez={'or_patterns':['aaa']})
+      for d in devices:
+          print(d)
+          if d.address == 'D5:5D:7C:31:DD:D5':
+            sd=d.details['props']['ServiceData']['0000fd3d-0000-1000-8000-00805f9b34fb']
+            print(''.join('{:02x}'.format(x) for x in sd))
+            ba = d.details['props']['ManufacturerData'][2409]
+            print(ba)
+            print(''.join('{:02x}'.format(x) for x in ba))
+            print(ba[9] - 128)
+            print(ba[8])
+            print(ba[10])
 
 asyncio.run(main())
