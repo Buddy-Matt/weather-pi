84d68b0ba822bd5a032105b0921201ed0325f3fe
Matt Gray <Matt@Matt-Gray.co.uk>
2023-06-04 22:39:21 +0100

    tester update

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ from bleak import BleakScanner
 async def main():
     while True:
       d = await BleakScanner.find_device_by_address('D5:5D:7C:31:DD:D5')
-      sd=d.details['props']['ServiceData']['0000fd3d-0000-1000-8000-00805f9b34fb']
-      print(''.join('{:02x}'.format(x) for x in sd))
+      print(d)
       ba = d.details['props']['ManufacturerData'][2409]
       print(ba)
       print(''.join('{:02x}'.format(x) for x in ba))
